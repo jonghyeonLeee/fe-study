fb9ae3f93c97f90820d575ce73878d3034df6969
jonghyeon <jonghyeonLee.90@gmail.com>
2024-03-31 20:31:04 +0900

    object destructuring

--- es6/03_destructuring_objects.js
+++ es6/03_destructuring_objects.js
@@ -2,6 +2,7 @@
     02 구조분해 할당
 
     ** 객체를 분해하여 필요한 값에 접근할 수 있음
+    ** 배열을 분해하여 할당할 수 있음
  */
 
 const employee = {
@@ -17,3 +18,7 @@ const {name, age, department: { designation }} = employee;
 console.log(name);
 console.log(age);
 console.log(designation);
+
+
+let [firstName, middleName, lastName] = ['Peter', 'John', 'Parkar'];
+console.log(lastName)// Parkar
